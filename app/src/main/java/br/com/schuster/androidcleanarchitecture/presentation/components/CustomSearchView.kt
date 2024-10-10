67cea8ba3cab5dd52bf349fbd0accd38844ef90e
cleberschuster <cleber.schuster.pro@gmail.com>
2024-10-10 09:48:32 -0300

    refactor textfield

--- app/src/main/java/br/com/schuster/androidcleanarchitecture/presentation/components/CustomSearchView.kt
+++ app/src/main/java/br/com/schuster/androidcleanarchitecture/presentation/components/CustomSearchView.kt
@@ -1,11 +1,9 @@
 package br.com.schuster.androidcleanarchitecture.presentation.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -17,38 +15,24 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CustomSearchView(
     search: String,
     modifier: Modifier = Modifier,
     onValueChange: (String) -> Unit,
-//    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    keyboardActions: KeyboardActions = KeyboardActions(),
     imeAction: ImeAction = ImeAction.Done,
     keyboardType: KeyboardType = KeyboardType.Number,
 ) {
 
-        TextField(value = search,
-            modifier = modifier
-
-            .clip(CircleShape)
-            .background(Color(0XFF101921)),
-            onValueChange = onValueChange,
-            colors = TextFieldDefaults.colors(
-                Color(0XFF101921),
-                Color(0XFF888D91),
-                Color(0XFF888D91),
-                Color(0XFF888D91),
-                Color.White,
-                focusedIndicatorColor = Color.Transparent, cursorColor = Color(0XFF070E14)
-            ),
-            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "") },
-//            trailingIcon = { Icon(imageVector = Icons.Default.Clear, contentDescription = "") },
-            placeholder = { Text(text = "Search") },
-            keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
-            keyboardActions = keyboardActions,
-        )
-
-
+    TextField(
+        value = search,
+        modifier = modifier
+            .clip(CircleShape.copy(all = CornerSize(10.dp))),
+        onValueChange = onValueChange,
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "") },
+        placeholder = { Text(text = "Search") },
+        keyboardOptions = KeyboardOptions(imeAction = imeAction, keyboardType = keyboardType),
+    )
 }

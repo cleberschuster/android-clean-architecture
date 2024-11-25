d8256bc279be87eac791a697dea9a2489f24146a
cleberschuster <cleber.schuster.pro@gmail.com>
2024-11-25 13:27:06 -0300

    remove unnecesary import

--- app/src/main/java/br/com/schuster/androidcleanarchitecture/presentation/feature/MainViewModel.kt
+++ app/src/main/java/br/com/schuster/androidcleanarchitecture/presentation/feature/MainViewModel.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow

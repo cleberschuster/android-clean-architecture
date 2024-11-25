72d589ac6c89b6e0872c9e65c1a02cc423098f69
cleberschuster <cleber.schuster.pro@gmail.com>
2024-11-25 13:22:56 -0300

    refact MainViewModel, removw collect() and add launchIn(viewModelScope)

--- app/src/main/java/br/com/schuster/androidcleanarchitecture/presentation/feature/MainViewModel.kt
+++ app/src/main/java/br/com/schuster/androidcleanarchitecture/presentation/feature/MainViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
@@ -44,16 +45,16 @@ class MainViewModel(private val useCase: PostUseCase) : ViewModel() {
             is MainScreenEvent.OnValueChange -> {
                 textSearch = event.searchText.trim()
             }
+
             is MainScreenEvent.OnSearch -> {
-                getNewComment(textSearch)
+                searchPosts()
             }
         }
+
     }
 
-    private fun getNewComment(id: String) {
-
+    private fun searchPosts() {
         viewModelScope.launch {
-
             if (textSearch.isBlank()) {
                 _uiEvent.send(UiEvent.ShowSnackbar(resId = R.string.search_not_empty))
 
@@ -65,37 +66,40 @@ class MainViewModel(private val useCase: PostUseCase) : ViewModel() {
                 return@launch
             }
 
-            _uiState.update { it.copy(status = Status.LOADING) }
+            getNewComment(textSearch)
+        }
+    }
 
-            delay(1000)
+    private suspend fun getNewComment(id: String) {
 
-            useCase.invoke(id.toInt())
+        _uiState.update { it.copy(status = Status.LOADING) }
+        delay(1000)
 
-                .onEach { result ->
+        useCase.invoke(id.toInt())
+            .onEach { result ->
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        status = Status.SUCCESS,
+                        data = result,
+                    )
+                }
+            }
+            .catch {
+                if (it.toErrorType().toString() == "404") {
                     _uiState.update { currentState ->
                         currentState.copy(
-                            status = Status.SUCCESS,
-                            data = result,
+                            status = Status.ERROR,
+                            errorMessage = it.toErrorType().toString()
+                        )
+                    }
+                } else {
+                    _uiState.update { currentState ->
+                        currentState.copy(
+                            status = Status.ERROR,
+                            errorMessage = it.toErrorType().toString()
                         )
                     }
                 }
-                .catch {
-                    if (it.toErrorType().toString() == "404") {
-                        _uiState.update { currentState ->
-                            currentState.copy(
-                                status = Status.ERROR,
-                                errorMessage = it.toErrorType().toString()
-                            )
-                        }
-                    } else {
-                        _uiState.update { currentState ->
-                            currentState.copy(
-                                status = Status.ERROR,
-                                errorMessage = it.toErrorType().toString()
-                            )
-                        }
-                    }
-                }.collect()
-        }
+            }.launchIn(viewModelScope)
     }
 }
